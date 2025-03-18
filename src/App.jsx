1b1462331dd97b8d35a61f17fe62da95ce75e974
bushraabir <muzahidulislamabir123@gmail.com>
2025-03-19 03:18:16 +0600

    Initial gh-pages branch

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import SpaceInvaders from './main/space';
+
+
+function App() {
+  return (
+    <div className="relative w-screen h-screen overflow-hidden">
+      <SpaceInvaders />
+    </div>
+  );
+}
+
+export default App;

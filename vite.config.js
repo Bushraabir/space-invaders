c11e8fe03ba6c4d0053c11e3b933dc62b4a01428
bushraabir <muzahidulislamabir123@gmail.com>
2025-03-19 03:09:34 +0600

    update for hosting

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,14 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-
   root: path.resolve(__dirname),
   plugins: [react()],
   resolve: {
-    
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
-   
     dedupe: ['react', 'react-dom'],
   },
+  
+  base: '/space-invaders/', 
 })
